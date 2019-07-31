a1031eb0bab2a20ecf06f432da21ef91fd26f055
anthony-baru <anthonybaru@gmail.com>
2019-07-31 20:50:02 +0300

    profile

--- business.php
+++ business.php
@@ -1,3 +1,9 @@
+<?php
+
+if (isset($_POST['business'])) { }
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -37,22 +43,20 @@
         <div class="row align-items-center">
           <div class="site-logo col-6"><a href="index.html">Reliable portal</a></div>
 
-            <nav class="mx-auto site-navigation">
-              <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-                <li><a href="index.html" class="nav-link active">Home</a></li>
-                <li><a href="job-listings.html">Hire Someone</a></li>
-                <li><a href="create_account.html">Create Account</a></li>
-                <li class="d-lg-none"><a href="contact.html">Contact Us</a></li>
-              </ul>
-            </nav>
+          <nav class="mx-auto site-navigation">
+            <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
+              <li><a href="index.html" class="nav-link active">Home</a></li>
+              <li><a href="job-listings.html">Hire Someone</a></li>
+              <li><a href="create_account.html">Create Account</a></li>
+              <li class="d-lg-none"><a href="contact.html">Contact Us</a></li>
+            </ul>
+          </nav>
 
           <div class="right-cta-menu text-right d-flex aligin-items-center col-6">
             <div class="ml-auto">
-              <a href="contact.html" class="btn btn-primary border-width-2 d-none d-lg-inline-block"><span
-                  class="mr-2 icon-paper-plane"></span>Contact Us</a>
+              <a href="contact.html" class="btn btn-primary border-width-2 d-none d-lg-inline-block"><span class="mr-2 icon-paper-plane"></span>Contact Us</a>
             </div>
-            <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span
-                class="icon-menu h3 m-0 p-0 mt-2"></span></a>
+            <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu h3 m-0 p-0 mt-2"></span></a>
           </div>
 
         </div>
@@ -60,8 +64,7 @@
     </header>
 
     <!-- HOME -->
-    <section class="home-section section-hero inner-page overlay bg-image"
-      style="background-image: url('images/.jpg');" id="home-section">
+    <section class="home-section section-hero inner-page overlay bg-image" style="background-image: url('images/.jpg');" id="home-section">
       <div class="container">
         <div class="row align-items-center justify-content-center">
           <div class="col-md-12">
@@ -76,8 +79,8 @@
 
 
 
-    
-    
+
+
     <section class="site-section pb-0">
       <div class="container">
         <div class="row align-items-center">
@@ -89,55 +92,61 @@
           </div>
           <div class="col-lg-6 ml-auto">
             <h2 class="section-title mb-3">Business Profile</h2><br>
+            <div class=" row form">
+              <form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                <?php
+                echo errorMessage();
+                echo successMessage();
+                if (!empty($errors)) {
+                  echo display_errors($errors);
+                }
+                ?>
+                <div class="form-row">
+                  <div class="form-group col-md-11">
+                    <label for="inputEmail4">Job Category</label>
+                    <select class="form-control" id="exampleFormControlSelect1" name="job_category">
+                      <option value="Mechanic">Mechanic</option>
+                      <option value="Floor Repir">Floor Repir</option>
+                      <option value="Painter">Painter</option>
+                      <option value="House Girl">House Girl</option>
+                      <option value="Chef">Chef</option>
+                      <option value="Electrician">Electrician</option>
+                      <option value="">Pest Control</option>
+                      <option value="Waiter">Waiter</option>
+                      <option value="Casual worker">Casual worker</option>
+                    </select>
+                  </div>
+                  <div class="form-group col-md-11">
+                    <label for="inputEmail4">Job Description</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" name="job_description" rows="3"></textarea>
+                  </div>
 
-              <div class=" row form">
-                <form>
-                  <div class="form-row">
-                    <div class="form-group col-md-11">
-                      <label for="inputEmail4">Job Category</label>
-                      <select class="form-control" id="exampleFormControlSelect1">
-                        <option>Mechanic</option>
-                        <option>Floor Repir</option>
-                        <option>Painter</option>
-                        <option>House Girl</option>
-                        <option>Chef</option>
-                        <option>Electrician</option>
-                        <option>Pest Control</option>
-                        <option>Waiter</option>
-                        <option>Casual worker</option>
-                      </select>
-                    </div>
-                    <div class="form-group col-md-11">
-                      <label for="inputEmail4">Job Description</label>
-                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    
-                    <div class="form-group col-md-7">
-                      <label for="inputAddress">Duration</label>
-                      <select class="form-control" id="exampleFormControlSelect1">
-                        <option>Choose...</option>
-                        <option>Full-Time</option>
-                        <option>Part-Time</option>
-                        <option>Free-Lancer</option>
-                        
-                      </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="inputPassword4">Salary (Kshs) per month</label>
-                      <input type="password" class="form-control" id="inputPassword4" placeholder="xxxx">
-                    </div>
-                    </div>
-                  <br><br>
-                  <button type="submit" class="btn btn-primary btn-mine">Submit</button>
-                </form>
-              </div>
-              </div>
+                  <div class="form-group col-md-7">
+                    <label for="inputAddress">Duration</label>
+                    <select class="form-control" id="exampleFormControlSelect1" name="type">
+                      <option>Choose...</option>
+                      <option>Full-Time</option>
+                      <option>Part-Time</option>
+                      <option>Free-Lancer</option>
 
+                    </select>
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputPassword4">Salary (Kshs) per month</label>
+                    <input type="number" class="form-control" id="inputPassword4" placeholder="xxxx" name="salary">
+                  </div>
+                </div>
+                <br><br>
+                <button type="submit" class="btn btn-primary btn-mine">Submit</button>
+              </form>
+            </div>
           </div>
+
         </div>
       </div>
-    </section>
-    <br><br><br><br><br><br>
+  </div>
+  </section>
+  <br><br><br><br><br><br>
 
   </div>
 
